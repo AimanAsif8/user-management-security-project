90125b25551132e98cc37a5231543768c57fefee
ASHWIN <meashwinclarence@gmail.com>
2024-05-13 13:26:21 +0530

    if user is deleted then user will be redirect to login if he is already logged in

--- controller/userController/homeController.js
+++ controller/userController/homeController.js
@@ -1,9 +1,16 @@
-const home=(req,res)=>{
+const userSchema = require("../../model/userSchema");
+
+const home=async (req,res)=>{
     try {
+        const userDetails=await userSchema.findOne({email:req.session.user})
+        if(userDetails===null){
+            req.session.user=""
+            return res.redirect('/user/login')
+        }
         res.render('user/home',{title:"Home",alertMessage:req.flash('errorMessage')})
     } catch (err) {
         console.log(`Error during home page render ${err}`);
-    }
+    }   
 }
 
 
